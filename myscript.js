8c3c9bee70766423fba2bbf2951b0aa830344df4
malapakavivek <malapakavivek628@gmail.com>
2024-12-22 16:32:30 +0530

    implemented rainbow color and clear functions

--- myscript.js
+++ myscript.js
@@ -6,15 +6,46 @@ function makeRows(rows,columns){
         container.appendChild(cell).className = "grid-item"
     }
 }
-
-
 makeRows(16,16)
 
+// background color changes to black on hovering
 const gridItems = document.querySelectorAll(".grid-item")
-function color(e){
-    e.target.style.backgroundColor = "black"
+const staticButton = document.querySelector(".color")
+
+function staticColor(){
+    function color(e){
+        e.target.style.backgroundColor = "black"
+    }
+    gridItems.forEach(item=> {
+        item.addEventListener("mouseover",color,false)
+    });
 }
 
-gridItems.forEach(item=> {
-    item.addEventListener("mouseover",color,false)
-});
+staticButton.addEventListener("click",staticColor,false)
+
+// background color changes to white when clicked clear (empty playground)
+const clearButton = document.querySelector(".clear")
+
+function clear(){
+    gridItems.forEach(item=> {
+        item.setAttribute("style","background-color:white")
+    });
+
+}
+clearButton.addEventListener("click",clear,false)
+
+//background color changes to random rainbow colors when clicked useRainbow 
+
+const rainbowButton = document.querySelector(".rainbow")
+let colors = {0:"violet",1:"indigo",2:"blue",3:"green",4:"yellow",5:"orange",6:"red"}
+
+function ClickRainbow(){
+    function Rainbow(e){
+        e.target.style.backgroundColor = colors[Math.floor(Math.random()*6)]
+    }
+    gridItems.forEach(item=> {
+        item.addEventListener("mouseover",Rainbow,false)
+    });
+}
+
+rainbowButton.addEventListener("click",ClickRainbow,false)
